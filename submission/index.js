1bb292e9ebce7187df2e9489bac0b56c6d0f3b91
David Scott <35239606+DavidRS91@users.noreply.github.com>
2018-04-23 11:01:57 -0700

    Setup mongo (#3)
    * connect to mongoDB, add new user endpoint, add db validation.
    
    * add music#new endpoint and validation for music, error handling

--- submission/index.js
+++ submission/index.js
@@ -2,14 +2,59 @@ const Koa = require("koa");
 const BodyParser = require("koa-bodyparser");
 const Logger = require("koa-logger");
 const Router = require("koa-router");
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/musicRecommendations");
+
+const Music = new Schema({
+  name: { type: String, required: true, unique: true },
+  tags: Array
+});
+
+const User = new Schema({
+  username: { type: String, required: true, unique: true },
+  listens: Array,
+  following: Array
+});
+
+Music.plugin(uniqueValidator);
+User.plugin(uniqueValidator);
+
+const MusicModel = mongoose.model("Music", Music);
+const UserModel = mongoose.model("User", User);
 
 const app = new Koa();
 const router = new Router();
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err;
+    ctx.app.emit("error", err, ctx);
+  }
+});
 
 router.get("/recommendations", async function(ctx) {
   ctx.body = {
     list: ["<music ID>", "<music ID>", "<music ID>", "<music ID>", "<music ID>"]
   };
+});
+
+router.post("/user/new", async function(ctx) {
+  let newUser = await new UserModel(ctx.request.body);
+  await newUser.save();
+  ctx.body = newUser;
+});
+
+router.post("/music/new", async function(ctx) {
+  let newUser = await new UserModel(ctx.request.body);
+  await newUser.save();
+  ctx.body = newUser;
 });
 
 router.post("/follow", async function(ctx) {
