8b59c8548986feb2a28b6bcaf3d25f59c8c57ebb
David Scott <35239606+DavidRS91@users.noreply.github.com>
2018-04-23 21:16:09 -0700

    add seed endpoint (#7)

--- submission/index.js
+++ submission/index.js
@@ -39,12 +39,6 @@ app.use(async (ctx, next) => {
   }
 });
 
-router.get("/recommendations", async function(ctx) {
-  ctx.body = {
-    list: ["<music ID>", "<music ID>", "<music ID>", "<music ID>", "<music ID>"]
-  };
-});
-
 router.post("/user/new", async function(ctx) {
   let newUser = await new UserModel(ctx.request.body);
   await newUser.save();
@@ -55,6 +49,93 @@ router.post("/music/new", async function(ctx) {
   let newMusic = await new MusicModel(ctx.request.body);
   await newMusic.save();
   ctx.body = newMusic;
+});
+
+router.post("/seed", async function(ctx) {
+  const { body } = ctx.request;
+  const numUsers = body.users || 10;
+  const numSongs = body.songs || 50;
+  let users = [],
+    songIds = [],
+    songs = [];
+  const tags = [
+    "60s",
+    "alternative",
+    "dance",
+    "electronic",
+    "instrumental",
+    "jazz",
+    "old school",
+    "pop",
+    "rock",
+    "samba"
+  ];
+
+  //clear any existing records
+  UserModel.collection.drop();
+  MusicModel.collection.drop();
+
+  //create desired number of songs with randomized tags
+  for (let i = 0; i < numSongs; i += 1) {
+    let genres = tags.filter(tag => Math.random() > 0.8);
+    if (genres.length === 0) {
+      genres.push("hipster");
+    }
+    songs.push({ name: `Song ${i}`, tags: genres });
+  }
+  //create desired number of users
+  for (let i = 0; i < numUsers; i += 1) {
+    users.push({ username: `User ${i}` });
+  }
+  //assign users to follow ~40% of other users
+  for (let user of users) {
+    user = user.following = users
+      .filter(u => Math.random() > 0.6 && u !== user)
+      .map(u => u.username);
+  }
+  //assign users to listen to 15 random songs
+  for (let i = 0; i < users.length; i += 1) {
+    let userSongs = [];
+
+    for (let i = 0; i < 15; i += 1) {
+      userSongs.push(songs[Math.floor(Math.random() * songs.length)]["name"]);
+    }
+    users[i]["listens"] = userSongs;
+  }
+
+  async function seed(users, songs) {
+    let userDocs = [];
+    for (const song of songs) {
+      const newSong = await new MusicModel(song).save();
+      song["id"] = newSong._id;
+      songIds.push(newSong._id);
+    }
+
+    for (const user of users) {
+      user["listens"] = user["listens"].map(
+        x => songs.filter(s => s.name === x).map(s => s.id)[0]
+      );
+      const newUser = await new UserModel(user);
+      userDocs.push(newUser);
+    }
+
+    for (const user of userDocs) {
+      user.following = user.following
+        .map(u => userDocs.filter(uD => uD["username"] === u))
+        .map(u => u[0]._id);
+      user.save();
+    }
+  }
+
+  seed(users, songs);
+
+  ctx.body = { users: users, songs: songs };
+});
+
+router.get("/recommendations", async function(ctx) {
+  ctx.body = {
+    list: ["<music ID>", "<music ID>", "<music ID>", "<music ID>", "<music ID>"]
+  };
 });
 
 router.post("/follow", async function(ctx) {
