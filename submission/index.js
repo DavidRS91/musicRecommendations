c82f0ca86094f6e818635eb24cae5a6329e878d7
David Scott <david.r.scott91@gmail.com>
2018-05-02 13:46:38 -0700

    store functions in lib

--- submission/index.js
+++ submission/index.js
@@ -58,54 +58,15 @@ app.use(async (ctx, next) => {
 router.get("/recommendations", async function(ctx) {
   const user = await UserModel.findOne({ username: ctx.query.user });
   const songs = await MusicModel.find({});
-  let top5Songs = [],
-    minScore = 2,
-    followsTagFreq = {};
-  let listensTagFreq = await tagFreq(user.listens);
-
-  for (let username of user.following) {
-    const followedUser = await UserModel.findOne({ username: username });
-    const followedUserTags = await tagFreq(
-      followedUser.listens,
-      followsTagFreq
-    );
-  }
-  Object.keys(followsTagFreq).forEach(
-    k => (followsTagFreq[k] /= user.following.length)
+  let userPreferences = await tagFreq(user.listens);
+  let followerPreferences = await followerFrequency(user.following);
+  let aggregatePreferences = aggregateFrequency(
+    userPreferences,
+    followerPreferences
   );
-
-  let avgTagFreq = listensTagFreq;
-  // aggregate tag frequency of listensTags and followsTags
-  Object.keys(followsTagFreq).forEach(
-    k =>
-      !!avgTagFreq[k]
-        ? (avgTagFreq[k] += followsTagFreq[k])
-        : (avgTagFreq[k] = followsTagFreq[k])
-  );
-
-  for (let song of songs) {
-    let songScore = song.tags // find average value of a song's tags in the avgTagFreq object
-      .map(t => (!!avgTagFreq[t] ? avgTagFreq[t] / song.tags.length : 0))
-      .reduce((a, b) => a + b);
-    for (let i = 0; i < 5; i += 1) {
-      if (
-        user.listens.indexOf(song.name) === -1 // check if user has heard song
-      ) {
-        if (top5Songs[i] === undefined || top5Songs[i][1] < songScore) {
-          //insert song into proper position of top5 songs
-          top5Songs.splice(i, 0, [song.name, songScore]);
-          if (top5Songs.length > 5) {
-            top5Songs.pop();
-          }
-          minScore = Math.min(minScore, songScore);
-          break;
-        }
-      }
-    }
-  }
 
   ctx.body = {
-    list: top5Songs.map(s => s[0])
+    list: sortSongs(songs, aggregatePreferences, user)
   };
 });
 
@@ -147,4 +108,62 @@ async function tagFreq(songs, tagWeighting = {}) {
     }
   }
   return tagWeighting;
+}
+
+async function followerFrequency(followedUsers) {
+  let followsTagFreq = {};
+  for (let username of followedUsers) {
+    const followedUser = await UserModel.findOne({ username: username });
+    const followedUserTags = await tagFreq(
+      followedUser.listens,
+      followsTagFreq
+    );
+
+    Object.keys(followsTagFreq).forEach(
+      k => (followsTagFreq[k] /= followedUsers.length)
+    );
+  }
+  return followsTagFreq;
+}
+
+function sortSongs(songs, aggregatePreferences, user) {
+  let top5Songs = [];
+  let minScore = 2;
+  for (let song of songs) {
+    let songScore = song.tags // find average value of a song's tags in the aggregatePreferences object
+      .map(
+        t =>
+          !!aggregatePreferences[t]
+            ? aggregatePreferences[t] / song.tags.length
+            : 0
+      )
+      .reduce((a, b) => a + b);
+    for (let i = 0; i < 5; i += 1) {
+      if (
+        user.listens.indexOf(song.name) === -1 // check if user has heard song
+      ) {
+        if (top5Songs[i] === undefined || top5Songs[i][1] < songScore) {
+          //insert song into proper position of top5 songs
+          top5Songs.splice(i, 0, [song.name, songScore]);
+          if (top5Songs.length > 5) {
+            top5Songs.pop();
+          }
+          minScore = Math.min(minScore, songScore);
+          break;
+        }
+      }
+    }
+  }
+  return top5Songs.map(s => s[0]);
+}
+
+function aggregateFrequency(pref1, pref2) {
+  let aggregatePreferences = pref1;
+  Object.keys(pref2).forEach(
+    k =>
+      !!aggregatePreferences[k]
+        ? (aggregatePreferences[k] += pref2[k])
+        : (aggregatePreferences[k] = pref2[k])
+  );
+  return aggregatePreferences;
 }
