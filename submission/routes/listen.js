20343f5bf6abb6eec865952c568befe40e738ec4
David Scott <david.r.scott91@gmail.com>
2018-05-03 13:40:40 -0700

    add 400 errors to handle bad requests

--- submission/routes/listen.js
+++ submission/routes/listen.js
@@ -6,13 +6,28 @@ const router = new Router({ prefix: "/listen" });
 
 router.post("/", async function(ctx) {
   const { body } = ctx.request;
+
+  if (typeof body.music !== "string" || typeof body.user !== "string") {
+    ctx.throw(400, "Invalid Input: from and to must be strings");
+  }
+
   const music = await MusicModel.findOne({ name: body.music });
-  const user1 = await UserModel.findOneAndUpdate(
+
+  if (!music) {
+    ctx.throw(400, "Invalid Input: Song not found");
+  }
+
+  const user = await UserModel.findOneAndUpdate(
     { username: body.user },
     { $push: { listens: music.name } },
     { new: true }
   );
-  ctx.body = user1;
+
+  if (!user) {
+    ctx.throw(400, "Invalid Input: User not found");
+  }
+
+  ctx.body = user;
 });
 
 module.exports = router;
